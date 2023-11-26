f692cb7eb36a060fc650ae69c982ccea7c34b88b
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-11-26 19:06:45 -0300

    Stage 3: Modified the implementation to handle different types of git objects (blob, commit) in GitObjectDataDisplayer class within git.kt file. Implemented methods to display these different git objects and detailed parsing for 'commit' objects including components like author name, timestamps, and commit messages. This results in more accurate and detailed git object data representation.

--- Git Internals/task/src/gitinternals/git.kt
+++ Git Internals/task/src/gitinternals/git.kt
@@ -3,6 +3,9 @@ package gitinternals
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,7 +28,77 @@ class GitObjectDataReader {
 class GitObjectDataDisplayer {
     fun display(data: String) {
         val type = data.substringBefore(' ')
-        val length = data.drop(type.length).substringBefore("\u0000").trim()
-        println("type:$type length:$length")
+
+        when (type) {
+            "blob" -> displayBlob(data)
+            "commit" -> displayCommit(data)
+            else -> println("Unknown git object type")
+        }
+    }
+
+    private fun displayBlob(data: String) {
+        val content = data.substringAfter("\u0000")
+        println("*BLOB*")
+        println(content)
+    }
+
+    private fun displayCommit(data: String) {
+        val payload = data.substringAfter("\u0000")
+        val lines = payload.split("\n")
+        val headers = lines.takeWhile { it.isNotBlank() }
+        val message = lines.dropWhile { it.isNotBlank() }.drop(1).joinToString("\n").trimEnd()
+
+        println("*COMMIT*")
+
+        var tree = ""
+        var parentLine = ""
+        var author = ""
+        var committer = ""
+        headers.forEach { line ->
+            when {
+                line.startsWith("tree") -> tree = line.substringAfter("tree ")
+                line.startsWith("parent") -> parentLine += "${line.substringAfter("parent ")} | "
+                line.startsWith("author") -> author = formatUserLine(line.substringAfter("author "))
+                line.startsWith("committer") -> committer = formatCommiter(line.substringAfter("committer "))
+            }
+        }
+
+        println("tree: $tree")
+
+        if (parentLine.isNotBlank()) {
+            println("parents: ${parentLine.dropLast(3)}")
+        }
+
+        println("author: $author")
+        println("committer: $committer")
+        println("commit message:")
+        println(message)
+    }
+
+
+    private fun formatUserLine(line: String): String {
+        val parts = line.split(" ")
+        val name = parts.takeWhile { !it.contains("@") }.joinToString(" ")
+        val email = parts.first { it.contains("@") }.removeSurrounding("<", ">")
+        val timestamp = parts[parts.size - 2].toLong()          // Unix timestamp
+        val timeZone = parts.last()
+
+        val formattedDate = Instant.ofEpochSecond(timestamp)
+            .atOffset(ZoneOffset.of(timeZone.replaceFirst(":", "")))  // Removing ":" to get valid timezone
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss XXX"))
+        return "$name $email original timestamp: $formattedDate"
+    }
+
+    private fun formatCommiter(line: String): String {
+        val parts = line.split(" ")
+        val name = parts.takeWhile { !it.contains("@") }.joinToString(" ")
+        val email = parts.first { it.contains("@") }.removeSurrounding("<", ">")
+        val timestamp = parts[parts.size - 2].toLong()          // Unix timestamp
+        val timeZone = parts.last()
+
+        val formattedDate = Instant.ofEpochSecond(timestamp)
+            .atOffset(ZoneOffset.of(timeZone.replaceFirst(":", "")))  // Removing ":" to get valid timezone
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss XXX"))
+        return "$name $email commit timestamp: $formattedDate"
     }
 }
